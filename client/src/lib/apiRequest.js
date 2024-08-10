f7f355ad0d73bbd15771a6eddc4f52f3a2961f10
Juswa <juswajuswagono@gmail.com>
2024-08-10 13:41:18 +0800

    URL UPDATED IN API REQUEST

--- client/src/lib/apiRequest.js
+++ client/src/lib/apiRequest.js
@@ -1,7 +1,9 @@
 import axios from 'axios'
 
+const API_URL = import.meta.env.MODE === "development" ? "http://localhost:5000/api" : "/api"
+
 const apiRequest = axios.create({
-    baseURL: "http://localhost:5000/api",
+    baseURL: API_URL,
     withCredentials: true
 })
 
